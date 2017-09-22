71d6b76dafcefdbb24465593b7aa28bad3bdb523
Aron <soha@fnal.gov>
2017-09-22 13:24:28 -0500

    Dataset dictionary file.

---
+++ Production/test/condorSub/dict_private_sig.py
@@ -0,0 +1,13 @@
+flist = {
+    "scenario": "Summer16sig",
+    "samples": [
+        ['Summer16_private.stealth_stop_350_singlino_SYY'],
+        ['Summer16_private.stealth_stop_350_singlino_SHuHd'],
+        ['Summer16_private.rpv_stop_350_t3j_uds'],
+        ['Summer16_private.rpv_stop_350_t3j_udb'],
+        ['Summer16_private.rpv_sbottom_350_t1j'],
+        ['Summer16_private.rpv_stop_450_t3j_uds'],
+        ['Summer16_private.rpv_stop_550_t3j_uds'],
+        ['Summer16_private.rpv_stop_650_t3j_uds'],
+    ]
+}
